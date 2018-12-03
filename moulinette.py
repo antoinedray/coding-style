0460ec7e56b723ff6bac25286fe3631c376c923d
Antoine Dray <antoine.dray@epita.fr>
2018-12-03 15:18:28 +0100

    Updates moulinette for comments styling error reporting

--- moulinette.py
+++ moulinette.py
@@ -26,6 +26,7 @@ class data:
     nb_line = 0
     len_count = 0
     nb_func = 0
+    nb_err = 0
     err = {}
 
 # ! CLASS DEFINITION
@@ -46,6 +47,7 @@ def match(line, regex):
     return 0
 
 def set_err(data, msg):
+    data.nb_err += 1
     if str(data.filename) in data.err:
         if str(data.nb_line) in data.err[data.filename]:
             data.err[str(data.filename)][str(data.nb_line)].append(msg)
@@ -57,12 +59,13 @@ def set_err(data, msg):
 
 def print_moulinette(data):
     for file in data.err:
-        print('\n' + info(('{ ' + file + ' }').center(80, '=')) + '\n')
+        print('\n' + info(('{ ' + file + ' }').center(80, '-')) + '\n')
         for line in data.err[file]:
             print(warn("Line: " + line))
             for msg in data.err[file][line]:
                 print('  ' + msg)
-    print()
+    print('\n' + fail(('{ STATS }').center(80, '=')) + '\n')
+    print('Total: ' + fail(str(data.nb_err)) + ' error found\n')
 
 def rule_braces(data):
     if match(data.line, "^([ ]*({|})(,|;)?)$"):
@@ -78,6 +81,16 @@ def rule_braces(data):
         return 0
     else:
         return 1
+
+def rule_multi_comments(data):
+    if match(data.line, "^(([ ]?\*[ ])|([ ]?\*$))"):
+        return 0
+    return 1
+
+def rule_no_space_before_comment(data):
+    if match(data.line, '^([ ]+[\*]+.*)'):
+        return 0
+    return 1
 
 def rule_cast(data):
     if match(data.line, "^.*=[ ]*[(].+([ ][*])?[)].+$"):
@@ -110,10 +123,14 @@ def rule_func_len(data):
 def rules(data):
     data, nb_func_rt = rule_func_num(data)
     data, len_func_rt = rule_func_len(data)
-    if not rule_cast(data):
-        data = set_err(data, 'No casts allowed. (8.1)')
+    if not rule_multi_comments(data):
+        data = set_err(data, 'Intermediary lines start with ** (5.9)')
+    if not rule_no_space_before_comment(data):
+        data = set_err(data, 'Comments must not be indented (5.9 bis)')
     if not rule_braces(data):
         data = set_err(data, 'All braces MUST be on their own line. (6.1)')
+    if not rule_cast(data):
+        data = set_err(data, 'No casts allowed. (8.1)')
     if not nb_func_rt:
         data = set_err(data, 'More than 10 functions in file. (8.9)')
     if not len_func_rt:
